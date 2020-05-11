29f813e4eb61dec5f66c685d355a9c383cdabe9d
Sebastianpobletejacob <sipoblete@uc.cl>
2020-05-11 15:48:12 -0400

    socket.io comillas

--- client.js
+++ client.js
@@ -1,9 +1,8 @@
-// import io from 'socket.io-client';
+import io from 'socket.io-client';
 
-// const socket = io('wss://le-18262636.bitzonte.com', {
-//   path: '/stocks',
-//   forceNew: true 
-// });
-// console.log(socket);
+const socket = io("wss://le-18262636.bitzonte.com", {
+  path: "/stocks"
+});
+console.log(socket);
 
-let socket = new WebSocket("wss://le-18262636.bitzonte.com/stocks");
+// let socket = new WebSocket("wss://le-18262636.bitzonte.com/stocks");
