a3b0ea60aff0c5a215bdf898d90672eea5e1032b
Sebastianpobletejacob <sipoblete@uc.cl>
2020-05-11 03:13:03 -0400

    websocket

--- client.js
+++ client.js
@@ -1,7 +1,14 @@
-import io from 'socket.io-client';
+// import io from 'socket.io-client';
 
-const socket = io('wss://le-18262636.bitzonte.com', {
-  path: '/stocks',
-  forceNew: true 
-});
-console.log(socket.id);
+// const socket = io('wss://le-18262636.bitzonte.com', {
+//   path: '/stocks',
+//   forceNew: true 
+// });
+// console.log(socket);
+
+let socket = new WebSocket("wss://le-18262636.bitzonte.com/stocks");
+
+socket.onopen = function(e) {
+  console.log("Socket on")
+  socket.send("My name is John");
+};
