c826d8bf761c03dc7e0a3aa05e344e1a86d2ca79
Sebastianpobletejacob <sipoblete@uc.cl>
2020-05-11 16:02:57 -0400

    comillasdobles

--- client.js
+++ client.js
@@ -1,8 +1,8 @@
-import io from 'socket.io-client';
+import io from "socket.io-client";
 
 const socket = io("wss://le-18262636.bitzonte.com", {
   path: "/stocks"
 });
-console.log(socket.id);
+console.log(socket);
 
 // let socket = new WebSocket("wss://le-18262636.bitzonte.com/stocks");
